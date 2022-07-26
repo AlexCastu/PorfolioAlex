2273ad1e01ee3c85e21b7865650839bc27354a3e
AlexCastu <alexcastu@gmail.com>
2022-07-26 14:35:58 +0200

    update

--- js/scroll.js
+++ js/scroll.js
@@ -2,3 +2,4 @@ ScrollReveal({ reset: true }).reveal('#headerContainer', { delay: 400 });
 ScrollReveal({ reset: true }).reveal('#seccionInicio', { delay: 400 });
 ScrollReveal({ reset: true }).reveal('#descripcionPersonal', { delay: 400 });
 ScrollReveal({ reset: true }).reveal('#seccionConocimientos', { delay: 400 });
+ScrollReveal({ reset: true }).reveal('#proyectos', { delay: 400 });
